0d68f0642ee4ffb975c8e75bc8800586502c3092
asad <jahar35@gmail.com>
2017-06-04 23:49:37 +0530

    menu successfully done

--- onlineshopping/src/main/java/com/asad/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/asad/onlineshopping/controller/PageController.java
@@ -9,7 +9,24 @@ public class PageController {
 	@RequestMapping(value={"/","/home","/index"})
 	public ModelAndView index(){
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring MVC");
+		mv.addObject("title", "home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+	
+	@RequestMapping(value="about")
+	public ModelAndView about(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value="contact")
+	public ModelAndView Contact(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 	/*@RequestMapping(value="/test")
